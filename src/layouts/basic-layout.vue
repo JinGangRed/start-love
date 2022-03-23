60961f711e699e9a4d5243e830976b1e8779d484
Kingkang <15950590137@163.com>
2022-03-23 16:17:46 +0800

    20220323

--- src/layouts/basic-layout.vue
+++ src/layouts/basic-layout.vue
@@ -1,6 +1,7 @@
 <template>
   <a-layout>
-    <a-layout-sider></a-layout-sider>
+    <a-layout-header>Header</a-layout-header>
+    <a-layout-content>Content</a-layout-content>
   </a-layout>
 </template>
 
--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,9 +1,13 @@
 <template>
   <left-menu-layout></left-menu-layout>
+  <basic-layout>
+    <slot />
+  </basic-layout>
 </template>
 
 <script setup lang="ts">
 import LeftMenuLayout from "./left-menu-layout.vue";
+import BasicLayout from "./basic-layout.vue";
 </script>
 
 <style scoped></style>
